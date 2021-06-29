7e4fd86078fa5243f0e696c61ad841a13d0b4b10
M1748595804 <1748595804@qq.com>
2021-06-29 21:30:28 +0800

    路由push、replace、编程式导航、withRouter、Prompt、、、、

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
     // console.log('我是一般组件', this.props);
     return <h2>React Router Demo</h2>;
   }
 }
+
+// 将一般组件转为路由组件
+// 高阶组件
+export default withRouter(Header);
--- src/pages/Home/Message/index.js
+++ src/pages/Home/Message/index.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch, Prompt } from 'react-router-dom';
 
 import Detail from './Detail';
 
 export default class Message extends Component {
   state = {
+    isGoBack: false,
     messageArr: [
       { id: '001', title: '消息1' },
       { id: '002', title: '消息2' },
       { id: '003', title: '消息3' },
     ],
   };
+  // 是否离开
+  blocking = () => {
+    console.log(1);
+    this.setState({ isGoBack: !this.state.isGoBack });
+  };
+
+  replaceShow = (id, title) => {
+    // replace跳转+params参数
+    // this.props.history.replace(`/home/message/detail/${id}/${title}`);
+
+    this.props.history.replace('/home/message/detail', { id, title });
+  };
+
+  pushShow = (id, title) => {
+    // push跳转+params参数
+    // this.props.history.push(`/home/message/detail/${id}/${title}`);
+
+    this.props.history.push('/home/message/detail', { id, title });
+  };
+  forword = () => {
+    this.props.history.goForward();
+  };
+  back = () => {
+    this.props.history.goBack();
+  };
+  go = () => {
+    this.props.history.go(-2);
+  };
+
   render() {
     const { messageArr } = this.state;
     return (
@@ -32,6 +62,12 @@ export default class Message extends Component {
                 >
                   {mes.title}
                 </Link>
+                <button className="btn btn-primary" onClick={() => this.pushShow(mes.id, mes.title)}>
+                  push查看
+                </button>
+                <button className="btn btn-primary" onClick={() => this.replaceShow(mes.id, mes.title)}>
+                  replace查看
+                </button>
               </li>
             );
           })}
@@ -39,10 +75,25 @@ export default class Message extends Component {
         <hr />
         <Switch>
           {/* 设置params参数 */}
-          <Route path="/home/message/detail/:id/:title" component={Detail}></Route>
+          {/* <Route path="/home/message/detail/:id/:title" component={Detail}></Route>s */}
           {/* search 设置 */}
           <Route path="/home/message/detail/" component={Detail}></Route>
         </Switch>
+        <button className="btn btn-primary" onClick={this.forword}>
+          前进
+        </button>
+        <button className="btn btn-primary" onClick={this.back}>
+          返回
+        </button>
+        <button className="btn btn-primary" onClick={this.go}>
+          go
+        </button>
+        {this.state.isGoBack.toString()}
+        {/* <Prompt when={this.state.isGoBack} message={(location) => `你确认想要去 ${location.pathname}`} /> */}
+        <Prompt when={this.state.isGoBack} message={(location) => `你确认想要去 ${location.pathname}`}/>
+        <button className="btn btn-danger" onClick={this.blocking}>
+          阻止离开
+        </button>
       </div>
     );
   }
