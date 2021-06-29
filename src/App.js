cc0a2a213da3231d544368f74cf35892aed2efb0
M1748595804 <1748595804@qq.com>
2021-06-29 14:46:50 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
-    return <div id="app"></div>;
+    return (
+      <div id="app">
+        <div>
+          <div className="row">
+            <div className="col-xs-offset-2 col-xs-8">
+              <div className="page-header">
+                <h2>React Router Demo</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-2 col-xs-offset-2">
+              <div className="list-group">
+                <a className="list-group-item active" href="./home.html">
+                  Home
+                </a>
+                <a className="list-group-item" href="./about.html">
+                  About
+                </a>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <div className="panel">
+                <div className="panel-body">
+                  <Home></Home>
+                  <About></About>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
